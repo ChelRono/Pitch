cb1205270581d6c46d4e024cb8625b7bdfe8402c
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-05-14 10:48:06 -0400

    added pitches

--- app.py
+++ app.py
@@ -1,6 +1,36 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 
+pitches= [
+    {
+        'author':'Valarie Rono',
+        'category':'pickup-lines',
+        'content':'I must be in a museum, because you truly are a work of art.',
+        'date-posted':'September 3rd, 2020'
+
+    },
+    {
+        'author':'Beatrice Rono',
+        'category':'Interview',
+        'content':'I’m Beatrice Rono, a lawyer with the government, based out of D.C. I grew up in Ohio, though, and I’m looking to relocate closer to my roots, and join a family-friendly firm. I specialize in labor law and worked for ABC firm before joining the government.',
+        'date-posted':'September 24th, 2019'
+
+    },
+    {
+        'author':'Sharon Rono',
+        'category':'Product',
+        'content':'For 130 years, Merck (known as MSD outside of the U.S. and Canada) has been inventing for life, bringing forward medicines and vaccines for many of the world’s most challenging diseases in pursuit of our mission to save and improve lives.',
+        'date-posted':'July 26th, 2021'
+    },
+        {
+        'author':'Victoria Rono',
+        'category':'Promotion',
+        'content':'m a sales rep at Better Than the Rest Cable. We help hotels across the U.S. pair with the perfect cable provider and plan for their region and needs. With regional experts assigned to each account, we help hotels identify the most cost-effective and guest-delighting cable plan for them.',
+        'date-posted':'August 27th, 2018'
+
+    }
+]
+
 
 @app.route("/")
 @app.route("/home")
