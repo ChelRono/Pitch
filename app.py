feb5dcc72598f838016774b97481bfb3106763aa
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-05-14 21:57:49 -0400

    added class user and class pitch

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
+from datetime import datetime
 from crypt import methods
 from turtle import title
 from flask import Flask,render_template, render_template, url_for, flash,redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY']='12345'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+db=SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 pitches= [
     {
@@ -60,13 +86,8 @@ def register():
 def login():
     form=LoginForm()
     if form.validate_on_submit():
-     if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-
-        flash(f'You have been logged in!', 'success')
+        flash(f'You have been logged in {form.email.data}!', 'success')
         return redirect(url_for('home'))
-    else:
-        flash('Login Unsuccessful. Please check username and password', 'danger')
-       
     return render_template('login.html',title='Login',form=form) 
 
 
