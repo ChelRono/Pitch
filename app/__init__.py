55e915001ca8797416e8262f57b793698dac18fb
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-05-15 09:36:11 -0400

    deployment

--- app/__init__.py
+++ app/__init__.py
@@ -16,10 +16,6 @@ db=SQLAlchemy(app)
 login_manager= LoginManager(app)
 bcrypt=Bcrypt(app)
 
-MAIL_SERVER = 'smtp.googlemail.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
 
 from app.main import views
